8717e8a9a3ed84ffd8a705e125453e1682f9bc37
fu-jun2 <fujun2@huawei.com>
2026-01-16 09:44:13 +0800

    【PR】: aclopDump接口迁移适配
    Co-authored-by: 傅骏<fujun2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !132 merge cherry-pick-mr-127-1768482961567-auto into 8.5.0
    
    【PR】: aclopDump接口迁移适配
    
    Created-by: fu-jun2
    Commit-by: 傅骏
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    aclopDump接口迁移到acl_dump.h中，acl_op.h需要相应适配。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [x] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    NA
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/ge!132

--- inc/external/acl/acl_op.h
+++ inc/external/acl/acl_op.h
@@ -13,6 +13,7 @@
 
 #include "acl_base.h"
 #include "acl_rt.h"
+#include "acl_dump.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -573,31 +574,6 @@ ACL_FUNC_VISIBILITY aclError aclopInferShape(const char *opType,
                                              int numOutputs,
                                              aclTensorDesc *outputDesc[],
                                              aclopAttr *attr);
-
-#define ACL_OP_DUMP_OP_AICORE_ARGS 0x00000001U
-
-/**
- * @ingroup AscendCL
- * @brief Enable the dump function of the corresponding dump type.
- *
- * @param dumpType [IN]       type of dump
- * @param path     [IN]       dump path
- *
- * @retval ACL_SUCCESS The function is successfully executed.
- * @retval OtherValues Failure
- */
-ACL_FUNC_VISIBILITY aclError aclopStartDumpArgs(uint32_t dumpType, const char *path);
-
-/**
- * @ingroup AscendCL
- * @brief Disable the dump function of the corresponding dump type.
- *
- * @param dumpType [IN]       type of dump
- *
- * @retval ACL_SUCCESS The function is successfully executed.
- * @retval OtherValues Failure
- */
-ACL_FUNC_VISIBILITY aclError aclopStopDumpArgs(uint32_t dumpType);
 
 #ifdef __cplusplus
 }
