c36d6ac689e118313401afaa9f020f55ea0ff408
gaoshuer913 <gaoshuer@huawei.com>
2026-02-05 21:23:07 +0800

    【PR】: fix: 删除st中的无效打印内容
    Co-authored-by: gaoshuer<gaoshuer@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !436 merge dev into develop
    
    【PR】: fix: 删除st中的无效打印内容
    
    Created-by: gaoshuer913
    Commit-by: gaoshuer
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    删除st中的无效打印内容
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    运行st: bf16_rsqrt_test, bf16_sin_test
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/ge!436

--- tests/autofuse/v35/st/backend_e2e_v2/bf16_sin_test/test_e2e_bf16_sin_kernel.cpp
+++ tests/autofuse/v35/st/backend_e2e_v2/bf16_sin_test/test_e2e_bf16_sin_kernel.cpp
@@ -41,7 +41,6 @@ TEST_P(E2E_BackendBf16Sin_Code, CalculateCorrect) {
   for (int i = 0; i < test_size; i++) {
     // Generate random float values
     double val = rand() / (double)RAND_MAX;
-    std::cout << val << " ";
     x[i] = static_cast<bfloat16_t>(val);
     // Calculate expected sin result
     expect[i] = static_cast<bfloat16_t>(std::sin(val));
@@ -57,10 +56,8 @@ TEST_P(E2E_BackendBf16Sin_Code, CalculateCorrect) {
 
   // Count difference
   uint32_t diff_count = 0;
-  std::cout << std::endl;
   for (int i = 0; i < test_size; i++) {
     if (std::fabs(y[i] - expect[i]) > 1e-2) {
-      std::cout << std::fabs(y[i] - expect[i]) << " ";
       diff_count++;
     }
   }
