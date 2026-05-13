33e33dd23aab714f834f905985d3fd8fab7a65f7
wanghuajie <wanghuajie1@huawei.com>
2026-05-13 20:27:52 +0800

    【style】: 解决编码规范问题，清除告警信息
    Co-authored-by: wanghuajie<wanghuajie1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2795 merge develop_remove_warning into develop
    
    【style】: 解决编码规范问题，清除告警信息
    
    Created-by: wanghuajie
    Commit-by: wanghuajie
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    去除包含的冗余头文件，解决编码规范问题，清楚告警
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [x] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1. 按照Readme文档编译ge仓代码，检查构建是否成功
    2. 执行UT和ST用例，检查是否全部通过
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/ge!2795

--- graph_metadef/graph/option/optimization_option_info.cc
+++ graph_metadef/graph/option/optimization_option_info.cc
@@ -13,7 +13,6 @@
 #include <unordered_set>
 #include "common/ge_common/debug/ge_log.h"
 #include "graph/ge_local_context.h"
-#include "external/ge_common/ge_api_types.h"
 
 namespace {
 const std::map<ge::OoLevel, std::string> kOoLevelStr = {{ge::OoLevel::kO1, "O1"}, {ge::OoLevel::kO3, "O3"}};
